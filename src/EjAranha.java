0beda56210f4e1929fd7350e33a6485ff06b4485
Martin-fb-28 <martinfbao@gmail.com>
2025-10-02 13:12:11 +0200

    ~https://github.com/Martin-fb-28/Ejercicio3.git

--- src/EjAranha.java
+++ src/EjAranha.java
@@ -14,11 +14,20 @@ public class EjAranha {
         System.out.println( nombre + " Entra en la clase de DAM muy silenciosamente y escala hasta el techo");
 
         System.out.println(" ¿Que hace Samuel tras entrar al aula? ");
+
+
         int respuesta = sc.nextInt();
 
-        // Investiga que hacen los alumnos y para hacerlo de la mejor manera posible se acerca a las pantallas colgandose de su tela
+        if (respuesta == 1) {
+System.out.println(" Investiga que hacen los alumnos y para hacerlo de la mejor manera posible se acerca a las pantallas colgandose de su tela ");
+        }else if (respuesta == 2) {
+            System.out.print( " Iván la mata ");
+        }
+
 
         System.out.println(" ¿Qué hace ahora? " + nombre);
+        System.out.println(" 1) trepa por la espalda de Jose Antonio y se esconde ");
+        System.out.println(" 2) se escapa por la ventana de busca de una nueva aventura ");
         int decision = sc.nextInt();
 
 
@@ -30,8 +39,8 @@ public class EjAranha {
             System.out.println(nombre + " se escapa por la ventana de busca de una nueva aventura ");
         }
     System.out.println( " Escoge un camino: ");
-    System.out.println("1. Al irse con Jose antonio lo descubren y se pelea ");
-    System.out.println("2. En vez de pelear se escapa ");
+    System.out.println(" 1. Al irse con Jose antonio lo descubren y se pelea ");
+    System.out.println(" 2. En vez de pelear se escapa ");
     int decision2 = sc.nextInt();
 
     if(decision2 == 1){
@@ -46,19 +55,26 @@ public class EjAranha {
     System.out.println( nombre + " escapa y sobrevive ");
         }
     System.out.println(" ¿Qué tiene en mente? " + nombre);
+    System.out.println(" 1) Encuentra una nueva aventura ");
+    System.out.println(" 2) vuelve a su hogar y descansa ");
     int decision3 = sc.nextInt();
 
   if (decision3 == 1 && decision2 == 2){;
             vida -= 0;
             distancia += 10;
             sobrevivio = true;
-            System.out.println( nombre + " Encuentra una nueva aventura ");
+            System.out.println( nombre + " Vive la vida hasta que la atropella un camión ");
         }
       if (decision3 == 2 && decision2 == 2) {
-          System.out.println( nombre + " vuelve a su hogar y descansa ");
+          System.out.println( nombre + " vuelve con su familia y vive felizmente ");
       }
    System.out.println(" Fin de esta aventura ");
 
+      //estadísticas
+
+    System.out.println(" La vida es:" + vida);
+    System.out.println(" La distancia es:" + distancia);
+    System.out.println(" La sobrevivio es:" + sobrevivio);
 
 
 
